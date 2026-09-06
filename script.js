07dadeb655e4182628b53916e5bb66a2732ec77e
Rohit Singh Sengar <rsengar257@gmail.com>
2026-09-06 21:48:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let codes=document.querySelectorAll('.code');
+if (codes.length > 0) {
+  codes[0].focus();
+}
+codes.forEach((currentInput,index)=>{
+	currentInput.addEventListener('input', (e) => {
+    if (currentInput.value.length === 1 && index < codes.length - 1) {
+      codes[index + 1].focus();
+    }
+  });
+  currentInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace') {    
+      if (currentInput.value === '' && index > 0) {
+        codes[index - 1].focus();
+        codes[index - 1].value = ''; // Optional: clears the previous field instantly
+      }
+    }
+  });
+})
